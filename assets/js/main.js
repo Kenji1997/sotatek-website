8d50499320c706fd3f0a57997abd74fcd2d347aa
bee <duy.ta@sotatek.com>
2018-11-05 09:32:32 +0700

    version 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,116 +1,166 @@
-jQuery(document).ready(function($) {
-	$('header, section, footer').addClass('hide');
-	setTimeout(()=>{
-		$('#preloader').addClass('hide');
-		$('body').removeClass('hide');
-	}, 2000)
+// jQuery(document).ready(function($) {
 
-	//trigger-mobile click
-	$('#trigger-mobile').on('click', function(event) {
-        $(this).toggleClass('active');
-        $('#header-menu').toggleClass('show');
-    });
-
-    $(window).on('scroll', function(event) {
-    	if ($(window).scrollTop() > 0) {
-	    	$('.header-01').addClass('scrolling');
-    	} else {
-	    	$('.header-01').removeClass('scrolling');
-    	}
-    });
-
-    $('#contact-form .input-item').click(function(event) {
-    	$(this).find('.label-01').addClass('up');
-    });
-
-	var swiper1 = new Swiper('.portpolio-swiper', {
-		slidesPerView: 2,
-		spaceBetween: 30,
-		loop: true,
-		centeredSlides: true,
-		grabCursor: true,
-		autoplay: 9000,
-		autoplayDisableOnInteraction: false,
-		autoplayStopOnLast : false,
-		pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-		},
-		navigation: {
-		nextEl: '.swiper-wrapper .swiper-slide .next',
-		prevEl: '.prev',
-		},
-		breakpoints: {
-			992: {
-			  	slidesPerView: 1,
-					grabCursor: false,
-			}
-		}
-	});
-
-	var swiper2 = new Swiper('.career-swiper', {
-		slidesPerView: 2,
-		spaceBetween: 30,
-		loop: true,
-		centeredSlides: true,
-		grabCursor: true,
-		autoplay: 9000,
-		autoplayDisableOnInteraction: false,
-		autoplayStopOnLast : false,
-		pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-		},
-		navigation: {
-		nextEl: '.swiper-wrapper .swiper-slide .next',
-		prevEl: '.prev',
-		},
-		breakpoints: {
-			992: {
-			  	slidesPerView: 1,
-					grabCursor: false,
-			}
-		}
-	});
-
-	var swiper3 = new Swiper('.office-swiper', {
-		slidesPerView: 3,
-		spaceBetween: 30,
-		loop: true,
-		autoplay: 5000,
-		autoplayDisableOnInteraction: false,
-		autoplayStopOnLast : false,
-		pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-		breakpoints: {
-			640: {
-			  	slidesPerView: 1,
-			},
-			992: {
-			  	slidesPerView: 2,
-			}
-		}
-	});
-
-	$('.slide-01 .swiper-wrapper .swiper-slide .button-item').on('click', function(event) {
-		event.preventDefault();
-		let t = $(this).find($('.prev')).length >0 ? 1 : 0;
-		if ( t == 1 ) {
-			swiper1.slidePrev();
-		} else {
-			swiper1.slideNext();
-		}
-	});
-});
+// });
 
 document.addEventListener('click', function(e){
-	let target = e.target;
-	let inputItem = document.getElementsByClassName('input-item');
-	for(var ele of inputItem){
-		if (!$(ele).is(target) && $(ele).has(target).length === 0 && $(ele).find('input').val().length == 0) {
-			$(ele).find('.label-01').removeClass('up');
-		}
-	}
+  // let target = e.target;
+  // let inputItem = document.getElementsByClassName('input-item');
+  // for(var ele of inputItem){
+  //  if (!$(ele).is(target) && $(ele).has(target).length === 0 && $(ele).find('input').val().length == 0) {
+  //    $(ele).find('.label-01').removeClass('up');
+  //  }
+  // }
 });
+
+window.addEventListener('DOMContentLoaded', function(){
+  // define Variable
+  var globalEvent = {
+    touch : false,
+    swipeLeft: false,
+    swipeRight: false,
+
+    reset : function(){
+      this.touch = false;
+      this.swipeLeft = false;
+      this.swipeRight = false;
+    }
+  }
+
+  var listTrack = document.getElementById('listTrack'),
+      speakerButton = document.getElementById('speaker-button'),
+      bulletListTrack = document.getElementById('list-track-bullet'),
+      bulletHome = document.getElementById('home-bullet'),
+      listBtn = document.getElementById('list-btn'),
+      gridBtn = document.getElementById('grid-btn'),
+      settingBtn = document.getElementById('settingBtn'),
+      modalSetting = document.getElementById('modalSetting'),
+      settingItem = document.getElementsByClassName('setting__item')
+      header = document.getElementById('header'),
+      playViewControl = document.getElementById('play-view__control'),
+      inputSearch = document.getElementsByClassName('input-search-01'),
+      searchResult = document.getElementById('search-result-wrapper'),
+      closeBtnModal = document.getElementsByClassName('close-btn')
+      ;
+
+  var mouseObject = {
+    posX: null,
+    posY: null,
+
+    setCurrentPos : function(x, y){
+      this.posX = x;
+      this.posY = y;
+    },
+
+    isSwipeLeft : function(x, y){ return x < (this.posX - 70) ? true : false},
+    isSwipeRight : function(x, y){ return x > (this.posX + 70 ) ? true : false},
+  }
+
+  const self = this;
+
+  //////////////////// define method
+  var isClick = e=> e.type === "click"?true : false;
+  var toggleModify = type=>{
+    console.log(type);
+    type === "grid" ? listTrack.classList.add('toggle-modify') : listTrack.classList.remove('toggle-modify');
+  }
+
+  var toggleClass = (element, className)=>{
+    console.log(element.classList.contains(className));
+    element.classList.contains(className) ? element.classList.remove(className) : element.classList.add(className);
+  }
+
+  var toggleMenu = ()=>{
+    toggleClass(header, "hide");
+  }
+
+  var toggleControl = ()=> {
+    toggleClass(playViewControl, "hide");
+  }
+
+  // handle listTrack event show & hide on mobile
+  const dashBoard = document.getElementsByClassName('Dashboard-01')[0];
+  dashBoard.addEventListener("touchstart", function(e){
+    let touches = e.changedTouches;
+    mouseObject.setCurrentPos( touches[0].pageX, touches[0].pageY );
+  });
+
+  dashBoard.addEventListener("touchmove", function(e){
+  })
+
+  dashBoard.addEventListener("touchend", function(e){
+    let touches = e.changedTouches;
+    if ( mouseObject.isSwipeLeft( touches[0].pageX, touches[0].pageY) ){
+      globalEvent.swipeLeft = true;
+      console.log("swipeLeft");
+    }
+
+    if ( mouseObject.isSwipeRight(touches[0].pageX, touches[0].pageY) ) {
+      globalEvent.swipeRight = true;
+      console.log("swipeRight");
+    }
+
+    // handle show list track
+    if (globalEvent.swipeRight) {
+      listTrack.classList.add('show');
+      bulletListTrack.classList.add('active');
+      bulletHome.classList.remove('active');
+    } else if (globalEvent.swipeLeft) {
+      listTrack.classList.remove('show');
+      bulletListTrack.classList.remove('active');
+      bulletHome.classList.add('active');
+    }
+
+    // reset globalEvent
+    globalEvent.reset();
+  });
+
+  // handle toggle speaker-button
+  speakerButton.addEventListener('click', function(){
+    this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
+  })
+
+  // handle toggle modify list track
+  listBtn.addEventListener('click', function(){
+    toggleModify('list');
+  })
+
+  gridBtn.addEventListener('click', function(){
+    toggleModify('grid');
+  })
+
+  // handle toggle setting
+  settingBtn.addEventListener('click', function(){
+    console.log('ok');
+    toggleClass(modalSetting, "show");
+  })
+
+  // handle setting
+  let settingItemArr = Array.from(settingItem);
+  settingItemArr.map(e=>{
+    let data = e.getAttribute("data-target");
+    console.log(data);
+    e.addEventListener('click', function(){
+      toggleClass(e, "active");
+      eval(data)();
+    })
+  })
+
+  // handle input search
+  let inputSearchArr = Array.from(inputSearch);
+  inputSearchArr[0].addEventListener('focus', function(){
+    searchResult.classList.add('show');
+  });
+
+  inputSearchArr[0].addEventListener('focusout', function(){
+    searchResult.classList.remove('show');
+  });
+
+  closeBtnModalArr = Array.from(closeBtnModal);
+  closeBtnModalArr.map(e=>{
+    e.addEventListener('click', function(){
+      if (e.closest('.modal-01')) {
+        e.closest('.modal-01').classList.remove('show');
+      }
+    })
+  })
+})
